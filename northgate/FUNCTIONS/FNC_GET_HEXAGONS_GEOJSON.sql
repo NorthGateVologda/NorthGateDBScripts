6d78ea211e5ab614af165907a41f8aca3bbb8eef
Artypm Shirkunov <shiae@localhost.localdomain>
2023-08-05 10:15:43 +0300

    Доработки

--- northgate/FUNCTIONS/FNC_GET_HEXAGONS_GEOJSON.sql
+++ northgate/FUNCTIONS/FNC_GET_HEXAGONS_GEOJSON.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE FUNCTION fnc_get_hexagons_geoJson() RETURNS JSON
                 json_agg(ST_AsGeoJSON(v.*)::json)
              ) AS geoJson
     FROM     (SELECT   pl.geometry AS hexagon,
-                       SUM(COALESCE(CAST(f.number_of_inhabitants AS DECIMAL), 0)) AS population
+                       FLOOR(SUM(COALESCE(CAST(f.number_of_inhabitants AS DECIMAL), 0))) AS population
               FROM     polygons_lens pl
                        LEFT JOIN facility_polygons fp
                           ON pl.id = fp.polygon_id
