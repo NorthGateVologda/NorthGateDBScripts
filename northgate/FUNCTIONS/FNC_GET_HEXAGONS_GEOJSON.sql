ffffdad44e13ec53cde3196f2ba84fac0d0761f7
shiae <kuni557929@gmail.com>
2023-08-09 00:06:33 +0300

    Добавил VIEW. Изменил функции

--- northgate/FUNCTIONS/FNC_GET_HEXAGONS_GEOJSON.sql
+++ northgate/FUNCTIONS/FNC_GET_HEXAGONS_GEOJSON.sql
@@ -1,19 +1,21 @@
 CREATE OR REPLACE FUNCTION fnc_get_hexagons_geoJson() RETURNS JSON
-	AS
-	$$
-	SELECT   json_build_object(
-                'type', 
-                'FeatureCollection',
-                'features', 
-                json_agg(ST_AsGeoJSON(v.*)::json)
-             ) AS geoJson
-    FROM     (SELECT   pl.geometry AS hexagon,
-                       FLOOR(SUM(COALESCE(CAST(f.number_of_inhabitants AS DECIMAL), 0))) AS population
-              FROM     polygons_lens pl
-                       LEFT JOIN facility_polygons fp
-                          ON pl.id = fp.polygon_id
-                       LEFT JOIN facility f
-                          ON fp.facility_id = f.facility_id
-              GROUP BY pl.geometry) v;
-	$$
-	LANGUAGE SQL;
+AS
+$$
+SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(v.*)::JSON)) AS geoJson
+FROM
+  (SELECT v.id,
+          v.hexagon,
+          v.population
+   FROM
+     (SELECT pl.id,
+             pl.geometry AS hexagon,
+             FLOOR(SUM(COALESCE(CAST(f.number_of_inhabitants AS DECIMAL), 0))) AS population,
+             COUNT(f.facility_id) AS number_of_buildings
+      FROM polygons_lens pl
+      LEFT JOIN facility_polygons fp ON pl.id = fp.polygon_id
+      LEFT JOIN facility f ON fp.facility_id = f.facility_id
+      GROUP BY pl.id,
+               pl.geometry) v
+   WHERE v.number_of_buildings > 0) v;
+$$
+LANGUAGE SQL;
